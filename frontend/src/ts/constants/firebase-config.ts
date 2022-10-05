ad860b3829bd5eddafa8ae916faf2d5b19637200
abscher <abscher.devops@gmail.com>
2022-10-05 16:06:49 +0300

    del gitignore

---
+++ frontend/src/ts/constants/firebase-config.ts
@@ -0,0 +1,13 @@
+// To find your config, go to https://console.firebase.google.com/ and select your project
+// Go to (top left) Settings > Project Settings > General
+// scroll down to Your apps > Web Apps (if it doesnt exist, create one) > SDK setup and configuration > select npm
+// your config should be visible there
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyASjNg12dAGmqPSQXm3n9F_16bXR6lXfTM",
+  authDomain: "monkeytype-8d52c.firebaseapp.com",
+  projectId: "monkeytype-8d52c",
+  storageBucket: "monkeytype-8d52c.appspot.com",
+  messagingSenderId: "620792939598",
+  appId: "1:620792939598:web:8f6154ef52cd93fbd42f1f"
+};
